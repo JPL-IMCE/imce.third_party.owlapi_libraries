b3ee4c2d1930c1b7b87cd0bc77541fc06b3f9938
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-07-24 23:57:18 +0000

    IMCEI-272 - Add connector to dump OTI MOF profiles/models for SPARK dependencies updates

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,13 +2,13 @@
 object Versions {
 
   // this project's version
-  val version = "2.3.3"
-  val scala_version = "2.11.7"
+  val version = "2.5.0"
+  val scala_version = "2.11.8"
 
   // owlapiLibs
 
   /** @see http://mvnrepository.com/artifact/net.sourceforge.owlapi/owlapi-distribution */
-  val owlapi = "4.1.3"
+  val owlapi = "5.0.2"
 
   /** @see http://mvnrepository.com/artifact/xml-resolver/xml-resolver/ */
   val xmlResolver = "1.2"
